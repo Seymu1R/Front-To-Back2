97d7e6dbe763ff64db6d9ea9842e99b3928bd7f6
Seymu1R <102611811+Seymu1R@users.noreply.github.com>
2022-10-26 02:23:36 +0400

    first commit

---
+++ Friont-To-Back/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Friont_To_Back.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Shop()
+        {
+            return View();
+        }
+    }
+}
---
+++ Friont-To-Back/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Friont_To_Back.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public double Price { get; set; }
+        public string?  Title { get; set; }
+    }
+}
